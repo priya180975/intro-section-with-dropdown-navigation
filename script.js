bd0cdfed4c750824c44016aa1e8562d12f923c06
Priya <priyannayak100@gmail.com>
2022-06-06 09:15:28 +0530

    adding script

---
+++ script.js
@@ -0,0 +1,46 @@
+let body=document.querySelector("body")
+
+let mainMenuOpenButton=document.querySelector(".menu__button")
+let mainMenuCloseButton=document.querySelector(".menu__cancel")
+let overlay=document.querySelector(".overlay")
+
+let mainMenuItems=document.querySelector(".menu__items")
+
+let featureSubmenuButton=document.querySelector(".feature__button")
+let companySubmenuButton=document.querySelector(".company__button")
+
+let featureSubmenuItems=document.querySelector(".menu__feature__items")
+let companySubmenuItems=document.querySelector(".menu__company__items")
+
+
+
+
+mainMenuOpenButton.addEventListener("click",openMainMenu)
+mainMenuCloseButton.addEventListener("click",closeMainMenu)
+
+featureSubmenuButton.addEventListener("click",featureSubmenu)
+companySubmenuButton.addEventListener("click",companySubmenu)
+
+function openMainMenu(){
+    mainMenuItems.style.display="block"
+    overlay.style.display="block"
+    body.style.overflow="hidden"
+    document.documentElement.scrollTop=0
+}
+
+function closeMainMenu(){
+    mainMenuItems.style.display="none"
+    overlay.style.display="none"
+    body.style.overflow="auto"
+}
+
+
+function featureSubmenu(){
+    featureSubmenuItems.classList.toggle("menu__submenu"); 
+    featureSubmenuButton.classList.toggle("opened")
+}
+
+function companySubmenu(){
+    companySubmenuItems.classList.toggle("menu__submenu"); 
+    companySubmenuButton.classList.toggle("opened")
+}
